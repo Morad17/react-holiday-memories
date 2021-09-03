5f69ddcdd968b11f22d84b647c5a5f51d3cc441e
Morad17 <morad123@hotmail.co.uk>
2021-09-03 23:22:09 +0100

    finished basic functionality

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose'
 import PostMessage from '../models/postMessage.js'
 
 
@@ -35,7 +36,29 @@ export const updatePost = async (req, res) => {
 
     if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with Id found')
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true })
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id}, { new: true })
 
     res.json(updatedPost)
+}
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with Id found')
+
+    await PostMessage.findByIdAndRemove(id)
+
+    res.json({ message: 'Post deleted successfully' })
+}
+
+export const likePost = async (req, res) => {
+
+    const { id } = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with Id found')
+
+    const post = await PostMessage.findById(id)
+    const likePost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1}, { new: true })
+
+    res.json(likePost)
 }

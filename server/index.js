f7f71dd73bc6f1518acfa944720ab14f22d409bb
Morad17 <morad123@hotmail.co.uk>
2021-09-03 23:43:13 +0100

    updates

--- server/index.js
+++ server/index.js
@@ -13,7 +13,11 @@ app.use(cors())
 
 app.use('/posts', postRoutes)
 
-const PORT = process.env.PORT || 5000
+app.get('/', (req, res) => {
+    res.sent('Welcome to the Holiday Memories Api')
+})
+
+const PORT = process.env.PORT
 
 mongoose.connect(process.env.CONNECTION_URL,{ useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
